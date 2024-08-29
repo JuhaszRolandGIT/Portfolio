53b6c638ed4dc8568af32fd32062f59eb44505da
Juhasz Roland <jszrl2003@gmail.com>
2024-08-29 15:35:13 +0200

    Add examples

---
+++ examples.js
@@ -0,0 +1,20 @@
+import componentsImg from './src/assets/components.png'
+import propsImg from './src/assets/config.png';
+import jsxImg from './src/assets/jsx-ui.png';
+import stateImg from './src/assets/state-mgmt.png';
+import React, { useState } from 'react';
+
+
+// data.js
+export const EXAMPLES = [
+  { title: "First Topic", 
+    description: "This is the content for the first topic." 
+  },
+
+  { title: "Second Topic", 
+    description: "This is the content for the second topic." 
+  },
+
+  { title: "Third Topic", 
+    description: "This is the content for the third topic." }
+];
