d125297a35928a85d9ce3b53597a3fb9e82e6a4e
Juhasz Roland <jszrl2003@gmail.com>
2024-09-01 18:16:41 +0200

    Add job information

---
+++ examples_jobs.js
@@ -0,0 +1,37 @@
+export const EXAMPLES = [
+  { 
+    title: "Ismert technológiák", 
+    description: `
+      <li>HTML5/CSS3/JavaScript</li>
+      <li>Bootstrap5</li>
+      <li>Vue.js</li>
+      <li>Angular</li>
+      <li>Node.js</li>
+      <li>PHP/Laravel</li>
+      <li>C#/.NET/WPF</li>
+      <li>MYSQL</li>
+      <li>GIT</li>
+    `
+  },
+
+  { 
+    title: "Tapasztalatok", 
+    description: `
+      <span style="color: #cf2c2c;">2024.06 - </span><br>Z-Bible Launchpad <br>
+      <span style="color: #cf2c2c;">2024.02 - 2024.07</span><br>FürgeWeb <br>
+      <span style="color: #cf2c2c;">2023.08 - 2024.01</span><br>Axioart Kft. <br>
+    ` 
+  },
+
+  {
+    title: "Tanulmányok", 
+    description: `
+      <span style="color: #cf2c2c;">2023 - </span><br>Győri SZC Jedlik Ányos Gépipari és Informatikai Technikum és Kollégium, Győr
+      <br><span style="color: grey; font-size: 13px">Szoftverfejlesztő- és tesztelő technikus</span><br>
+      <span style="color: #cf2c2c;">2019 - 2023</span><br>Komáromi Jókai Mór Gimnázium<br>
+      <span style="color: grey; font-size: 13px">Érettségi</span><br>
+
+    ` 
+  }
+  
+];
