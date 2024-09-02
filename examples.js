7524372549f0b5bc78d86af992a8d95ee69a172a
Juhasz Roland <jszrl2003@gmail.com>
2024-09-02 18:51:07 +0200

    Job info integration

--- examples.js
+++ examples.js
@@ -1,20 +1,57 @@
-import componentsImg from './src/assets/components.png'
-import propsImg from './src/assets/config.png';
-import jsxImg from './src/assets/jsx-ui.png';
-import stateImg from './src/assets/state-mgmt.png';
-import React, { useState } from 'react';
-
-
-// data.js
 export const EXAMPLES = [
-  { title: "First Topic", 
-    description: "This is the content for the first topic." 
+  { 
+    title: "Ismert technológiák", 
+    description: `
+    <div class="li-style">
+      <li>HTML5/CSS3/JavaScript</li>
+      <li>Bootstrap5</li>
+      <li>Vue.js</li>
+      <li>Angular</li>
+      <li>Node.js</li>
+      <li>PHP/Laravel</li>
+      <li>C#/.NET/WPF</li>
+      <li>MYSQL</li>
+      <li>GIT</li>
+    </div>
+    `
   },
 
-  { title: "Second Topic", 
-    description: "This is the content for the second topic." 
+  { 
+    title: "Tapasztalatok", 
+    jobs: [
+      { 
+        name: "Z-Bible Launchpad", 
+        date: "2024.06 - ",
+        details: "Z-Bible Launchpad",
+        info: `A modernizált biblia szoftver fejlesztésében dolgozom, ahol a frontend feladatok végrehajtása tartozik hozzám. 
+    Feladataim közé tartozik a felhasználói felület optimalizálása és a felhasználói élmény javítása a projekt célkitűzéseinek megfelelően.
+`
+      },
+      { 
+        name: "FürgeWeb", 
+        date: "2024.02 - 2024.07",
+        details: "Fürgeweb",
+        info: `A kisebb vállalat keretein belül az ügyfelek igényeihez illeszkedő weboldalak tervezésével és fejlesztésével foglalkoztam.
+     Feladataim közé tartoztak a projekt-specifikus követelmények pontos teljesítése és a felhasználói élmény optimalizálása.`
+      },
+      { 
+        name: "Axioart Kft.", 
+        date: "2023.08 - 2024.01",
+        details: "Axioart Kft.",
+        info: `Excel, Word és Access programok használatával foglalkoztam, különös figyelmet fordítva az adatok pontos rögzítésére és azok hatékony bevitelére az adatbázisokba. 
+    Feladataim közé tartoztak az adatkezelési folyamatok optimalizálása és a dokumentációs feladatok precíz végrehajtása.
+    ` 
+      }
+    ]
   },
 
-  { title: "Third Topic", 
-    description: "This is the content for the third topic." }
+  {
+    title: "Tanulmányok", 
+    description: `
+      <span style="color: #cf2c2c;">2023 - </span><br>Győri SZC Jedlik Ányos Gépipari és Informatikai Technikum és Kollégium, Győr
+      <br><span style="color: grey; font-size: 13px">Szoftverfejlesztő- és tesztelő technikus</span><br>
+      <span style="color: #cf2c2c;">2019 - 2023</span><br>Komáromi Jókai Mór Gimnázium<br>
+      <span style="color: grey; font-size: 13px">Érettségi</span><br>
+    ` 
+  }
 ];
